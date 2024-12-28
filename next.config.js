56d368e0604a7c3b889fe63f291f29b9666ac62d
Roderick Moise <roderickfmoise@gmail.com>
2024-12-28 20:38:37 +0100

    fix: convert next.config.ts to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['ik.imagekit.io', 'cdn.builder.io'],
@@ -18,4 +17,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
